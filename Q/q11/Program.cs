30b6d799b77319aafd6979367fb9722b9e076df4
Sergey-2021 <tatarinov.serzhick@yandex.ru>
2021-11-07 21:22:49 +0300

    Задача 11

---
+++ Q/q11/Program.cs
@@ -0,0 +1,17 @@
+﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+int num = new Random().Next(10, 100);
+int a = num % 10;
+int b = num / 10;
+Console.WriteLine(" ");
+Console.WriteLine("Дано число из отрезка [10, 99]. Показать наибольшую цифру числа:");
+Console.WriteLine(" ");
+Console.WriteLine($"Число из отрезка [10, 99] - {num}");
+Console.WriteLine(" ");
+if (a > b)
+{
+    Console.WriteLine($"Наибольшуая цифра числа: {a}");
+}
+else
+{
+    Console.WriteLine($"Наибольшуая цифра числа: {b}");
+}
