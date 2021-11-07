e70401473ce94d15a7cd73843bceeb317f165d34
Sergey-2021 <tatarinov.serzhick@yandex.ru>
2021-11-07 22:41:48 +0300

    Задача 12

---
+++ Q/q12/Program.cs
@@ -0,0 +1,8 @@
+﻿// Удалить вторую цифру трёхзначного числа
+int number = new Random().Next(100, 1000);
+int a = number%10;
+int b = number/100*10+a;
+Console.WriteLine(" ");
+Console.WriteLine($"Удалить вторую цифру трёхзначного числа: {number}");
+Console.WriteLine($"Результат удаления второй цифры трехзначного числа - {b}");
+Console.WriteLine(" ");
