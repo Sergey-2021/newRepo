ffef942083dd03ce58b4ff1e843e9e3765929d9b
Sergey-2021 <tatarinov.serzhick@yandex.ru>
2021-11-08 21:27:48 +0300

    переименовал файл

---
+++ Q/q12/EX_12.cs
@@ -0,0 +1,12 @@
+﻿// Удалить вторую цифру трёхзначного числа
+int number = new Random().Next(100, 1000);
+int D (int x)
+{
+int a = number%10;
+int b = number/100*10+a;
+return b;
+}
+Console.WriteLine();
+Console.WriteLine($"Удалить вторую цифру трёхзначного числа: {number}");
+Console.WriteLine($"Результат удаления второй цифры трехзначного числа - {D(number)}");
+Console.WriteLine();
