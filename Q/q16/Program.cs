68d738edd16ddfc4e4bf39bfb099b4d7408f20b0
Sergey-2021 <tatarinov.serzhick@yandex.ru>
2021-11-10 21:34:20 +0300

    задача 16

---
+++ Q/q16/Program.cs
@@ -0,0 +1,16 @@
+﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+
+
+bool DayNum(int x)
+{
+    return (x == 6 | x == 7);
+}
+int day = int.Parse(Console.ReadLine());
+if (day >= 1 & day <= 7)
+{
+    Console.WriteLine(DayNum(day));
+}
+else
+{
+    Console.WriteLine("Увы.");
+}
