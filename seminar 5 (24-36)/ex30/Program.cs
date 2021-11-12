c5e1433a78d5e3feff2f32d22eaff05e873803d6
Sergey-2021 <tatarinov.serzhick@yandex.ru>
2021-11-12 07:32:39 +0300

    задача 30

---
+++ seminar 5 (24-36)/ex30/Program.cs
@@ -0,0 +1,22 @@
+﻿//Показать кубы чисел, заканчивающихся на четную цифру
+int num = 7;
+int UU(int x)
+{
+    int r = 1;
+    for (int i = 1; i <= x; i++) 
+    {
+        r = i * i * i;
+    }
+    return r;
+}
+for (int i = 1; i < +num; i++)
+{
+   System.Console.WriteLine(UU(i));
+}
+System.Console.WriteLine();
+for (int i = 1; i < +num; i++)
+{
+    int t = UU(i);
+    if (t % 2 == 0) System.Console.WriteLine(t);
+}
+
