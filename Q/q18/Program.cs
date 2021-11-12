3dada009fe9709e44f68c6290a2a1fcdb4f8d1a2
Sergey-2021 <tatarinov.serzhick@yandex.ru>
2021-11-12 07:52:34 +0300

    задача 18

--- Q/q18/Program.cs
+++ Q/q18/Program.cs
@@ -6,16 +6,7 @@ bool T(bool x, bool y)
 }
 bool x = false;
 bool y = true;
-// bool x = false;
-// bool y = false;
-// bool x = true;
-// bool y = true;
-// bool x = true;
-// bool y = false;
-bool a = !(x | y);
-bool b = !x & !y;
-bool t = a == b;
-Console.WriteLine("¬(X | Y)" + " " + "=" + " " + a);
-Console.WriteLine("¬X & ¬Y" + " " + "=" + " " + b);
+
+
 Console.WriteLine("Функция" + " " + T(x, y));
-Console.WriteLine("Сравнение в переменной t" + " " + t);
+
