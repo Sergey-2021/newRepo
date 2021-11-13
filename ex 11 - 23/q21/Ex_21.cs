b1ef36c6b11e19f632d9ef16dca54de85a9717aa
Sergey-2021 <tatarinov.serzhick@yandex.ru>
2021-11-13 22:23:35 +0300

    задача 21

---
+++ ex 11 - 23/q21/Ex_21.cs
@@ -0,0 +1,16 @@
+﻿// Программа проверяет пятизначное число на палиндромом.
+System.Console.Write("Введите пятизначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+bool Pal(int x)
+{
+    if (x % 10 == x / 10000 & x % 100 / 10 == x / 1000 % 10)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+if (Pal(num)) System.Console.WriteLine($"Число {num} - палиндром.");
+else System.Console.WriteLine($"Число {num} - не палиндром.");
