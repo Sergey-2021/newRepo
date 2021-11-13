9d4c9d3dad7908db914e1ba3c9d23b417ed3f367
Sergey-2021 <tatarinov.serzhick@yandex.ru>
2021-11-13 21:39:29 +0300

    задача 18

---
+++ ex 11 - 23/q18/Program.cs
@@ -0,0 +1,12 @@
+﻿// Проверить истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y
+bool T(bool x, bool y)
+{
+   bool r = (!(x || y)) == (!x && !y);
+        return r;
+}
+bool x = false;
+bool y = true;
+
+
+Console.WriteLine("Функция" + " " + T(x, y));
+
