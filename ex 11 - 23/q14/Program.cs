58a1ce97729fcc2aa5e5b0c4e407ce430d29e719
Sergey-2021 <tatarinov.serzhick@yandex.ru>
2021-11-13 21:25:18 +0300

    задача 14

---
+++ ex 11 - 23/q14/Program.cs
@@ -0,0 +1,13 @@
+﻿// Найти третью цифру числа или сообщить, что её нет
+int num = new Random().Next(90, 111);
+Console.WriteLine(" ");
+if (num < 100)
+{
+    Console.WriteLine($"Число {num} не содержит третью цифру.");
+}
+else
+{
+    Console.WriteLine($"{num % 10} - третья цифра числа {num}");
+}
+
+Console.WriteLine(" ");
