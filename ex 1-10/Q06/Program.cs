976f4e77a7de9d4f8b54e25c1fd7bff58c57a839
Sergey-2021 <tatarinov.serzhick@yandex.ru>
2021-11-13 21:19:40 +0300

    задача 6

---
+++ ex 1-10/Q06/Program.cs
@@ -0,0 +1,16 @@
+﻿// Выяснить является ли число чётным
+void EvenNumber(int x)
+{
+    
+    if(x%2==0) Console.WriteLine($"Число {x} четное.");
+    else
+    {
+        Console.WriteLine("Вы ввели не четное число.");
+    }
+}
+Console.WriteLine(" ");
+Console.WriteLine("Выяснить является ли число чётным: ");
+Console.Write("Ведите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+EvenNumber(number);
+Console.WriteLine(" ");
