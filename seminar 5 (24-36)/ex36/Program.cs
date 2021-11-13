3358bc0e93d194e6241f82420d3493f8f14bbedd
Sergey-2021 <tatarinov.serzhick@yandex.ru>
2021-11-13 21:15:02 +0300

    задача 36

---
+++ seminar 5 (24-36)/ex36/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задать массив, заполнить случайными положительными трёхзначными числами.
+// Показать количество нечетныхчетных чисел.
+int[] arrey = new int[10];
+int[] FillArrey(int[] x)
+{
+    for (int i = 0; i < x.Length; i++)
+    {
+        x[i] = new Random().Next(0, 100);
+    }
+    return x;
+}
+FillArrey(arrey);
+int count = 0;
+for (int i = 0; i < arrey.Length; i++)
+{
+    if (arrey[i] % 2 != 0 )
+        count++;
+    System.Console.Write(arrey[i] + " ");
+}
+System.Console.WriteLine();
+System.Console.WriteLine("Hечетныхчетных чисел в массиве - " + count);
