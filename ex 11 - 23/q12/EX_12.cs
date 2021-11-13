510717cacfb77b79aeea7be98878438b14423037
Sergey-2021 <tatarinov.serzhick@yandex.ru>
2021-11-13 21:24:36 +0300

    задача 12

---
+++ ex 11 - 23/q12/EX_12.cs
@@ -0,0 +1,13 @@
+﻿// Удалить вторую цифру трёхзначного числа
+Console.Clear();
+int number = new Random().Next(100, 1000);
+int D (int x)
+{
+int a = number%10;
+int b = number/100*10+a;
+return b;
+}
+Console.WriteLine();
+Console.WriteLine($"Удалить вторую цифру трёхзначного числа: {number}");
+Console.WriteLine($"Результат удаления второй цифры трехзначного числа - {D(number)}");
+Console.WriteLine();
