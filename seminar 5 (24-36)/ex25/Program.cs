3b2646b4c3bc7338ee6ef07d0a8d0660890782aa
Sergey-2021 <tatarinov.serzhick@yandex.ru>
2021-11-14 22:08:19 +0300

    задача 25

---
+++ seminar 5 (24-36)/ex25/Program.cs
@@ -0,0 +1,12 @@
+﻿// Найти сумму чисел от 1 до А
+int num = int.Parse(Console.ReadLine());
+int Get(int x)
+{
+    int sum = 0;
+    for (int i = 1; i <= x; i++)
+    {
+        sum = sum + i;
+    }
+    return sum;
+}
+System.Console.WriteLine(Get(num));
