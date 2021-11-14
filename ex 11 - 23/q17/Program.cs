1ef49c30722a5628cbbdcda7e6e8baed3581deb7
Sergey-2021 <tatarinov.serzhick@yandex.ru>
2021-11-14 08:13:27 +0300

    задача 17

---
+++ ex 11 - 23/q17/Program.cs
@@ -0,0 +1,21 @@
+﻿// По двум заданным числам проверять является ли одно квадратом другого
+System.Console.WriteLine();
+System.Console.Write("Введите первое число: ");
+int one =Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите второе число: ");
+int two = Convert.ToInt32(Console.ReadLine());
+string Get(int x, int y)
+{ 
+    if(x*x==y)
+    return ($"{y} является квадратом {x}");
+    else if(y*y==x)
+    {
+        return ($"{x} является квадратом {y}");
+    }
+    else
+    {
+        return "Ни одно число не является квадратом другого";
+    }
+}  
+System.Console.WriteLine();
+System.Console.WriteLine(Get(one, two));
