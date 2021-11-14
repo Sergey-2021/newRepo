44539c55026d05a0dafae31df80548fd5b275e14
Sergey-2021 <tatarinov.serzhick@yandex.ru>
2021-11-14 08:52:19 +0300

    задача 20

---
+++ ex 11 - 23/q20/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задать номер четверти, показать диапазоны для возможных координат
+
+string Get(int x, int y)
+{
+
+    if (x > 0 & y > 0)
+    {
+        return "Первая четверть. X(от 0 до n) и Y(от 0 до n)";
+    }
+    else if (x < 0 & y > 0)
+    {
+        return "Вторая четверть. X(-n до 0) и Y(от 0 до n)";
+    }
+    else if (x < 0 & y < 0)
+    {
+        return "Третья четверть. X(-n до 0) и Y(-n до 0)";
+    }
+    else if (x == 0 | y == 0)
+    {
+        return "Введите числа не равные 0";
+    }
+    else 
+    {
+        return "Четвертая четверть. X(от 0 до n) и Y(-n до 0)";
+    }
+}
+
+System.Console.Write("Введите координату X: ");
+int x = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите координату Y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(Get(x, y));
