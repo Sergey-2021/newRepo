f313c68bdb3a3e09e5f20bbfeabf10c57b08f191
Sergey-2021 <tatarinov.serzhick@yandex.ru>
2021-11-14 19:15:21 +0300

    задача 23

---
+++ ex 11 - 23/q23/Program.cs
@@ -0,0 +1,11 @@
+﻿//Показать таблицу квадратов чисел от 1 до N
+int Get(int x)
+{
+    x=x*x;
+    return x;
+}
+int num = Convert.ToInt32(Console.ReadLine());
+for(int i = 1; i<=num; i++)
+{
+    System.Console.WriteLine($"Квадрат числа {i} = {Get(i)}");
+}
