4b99d826e8cb3ef828f34f87a81a125c42b43b24
Sergey-2021 <tatarinov.serzhick@yandex.ru>
2021-11-14 22:01:20 +0300

    задача 24

---
+++ seminar 5 (24-36)/ex24/Program.cs
@@ -0,0 +1,10 @@
+﻿// Найти кубы чисел от 1 до N
+int num = int.Parse(Console.ReadLine());
+int Get(int a)
+{
+    return a*a*a;
+}
+for (int i = 1; i <= num; i++)
+{
+    System.Console.WriteLine(Get(i));
+}
