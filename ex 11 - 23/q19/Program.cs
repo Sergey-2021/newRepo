8e035cb8af787ee69782436e6561eac85b1c38ba
Sergey-2021 <tatarinov.serzhick@yandex.ru>
2021-11-14 08:40:53 +0300

    задача 19

---
+++ ex 11 - 23/q19/Program.cs
@@ -0,0 +1,33 @@
+﻿// Определить номер четверти плоскости, 
+//в которой находится точка с координатами Х и У, причем X ≠ 0 и Y ≠ 0
+
+string Get(int x, int y)
+{
+
+    if (x > 0 & y > 0)
+    {
+        return "Первая четверть";
+    }
+    else if (x < 0 & y > 0)
+    {
+        return "Вторая четверть";
+    }
+    else if (x < 0 & y < 0)
+    {
+        return "Третья четверть";
+    }
+    else if (x == 0 | y == 0)
+    {
+        return "Введите числа не равные 0";
+    }
+    else 
+    {
+        return "Четвертая четверть";
+    }
+}
+
+System.Console.Write("Введите координату X: ");
+int x = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите координату Y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(Get(x, y));
