3dc6a8dd4a5419baa67d77816d9a618276ee6051
Sergey-2021 <tatarinov.serzhick@yandex.ru>
2021-11-15 08:43:43 +0300

    задача 27

---
+++ seminar 5 (24-36)/ex27/Program.cs
@@ -0,0 +1,11 @@
+﻿// // Определить количество цифр в числе
+System.Console.WriteLine();
+System.Console.Write("Введите любое число: ");
+string num = Console.ReadLine();
+int GetNum(string x)
+{
+    int d = x.Length;
+    return d;
+}
+
+System.Console.WriteLine($"Количество цифр в числе {num} = {GetNum(num)}");
