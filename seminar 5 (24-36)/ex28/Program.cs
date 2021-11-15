8e65a6faeb5ad4440c46cf77c6f6d2e1d9081e52
Sergey-2021 <tatarinov.serzhick@yandex.ru>
2021-11-15 10:38:48 +0300

    задача 28

---
+++ seminar 5 (24-36)/ex28/Program.cs
@@ -0,0 +1,19 @@
+﻿// Подсчитать сумму цифр в числе
+System.Console.WriteLine();
+System.Console.Write("Введите число (не более 9 символов): ");
+int num = Convert.ToInt32(System.Console.ReadLine());
+int GetSum(int x)
+{
+    string u = x.ToString();
+    int sum = 0;
+    for (int i = u.Length; i > 0; i--)
+    {
+        if (i > 0)
+        {
+            sum += x % 10;
+            x = x / 10;
+        }
+    }
+    return sum;
+}
+System.Console.WriteLine($"Сумма цифр числа {num} = {GetSum(num)}");
