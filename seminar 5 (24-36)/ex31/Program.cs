4f617a9d8273bd00469cfee26c6baa41151edf11
Sergey-2021 <tatarinov.serzhick@yandex.ru>
2021-11-15 11:02:18 +0300

    задача 31

---
+++ seminar 5 (24-36)/ex31/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задать массив из 8 элементов и вывести их на экран
+int[] array = new int[8];
+int[] FillArr(int[] x)
+{
+    for (int i = 0; i < x.Length; i++)
+    {
+        x[i] = new Random().Next(0, 10);
+    }
+    return x;
+}
+void Print(int[] x)
+{
+    for (int i = 0; i < x.Length; i++)
+    {
+        System.Console.Write(x[i]+" ");
+    }
+}
+array = FillArr(array);
+Print(array);
