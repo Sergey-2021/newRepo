91a849eab93a7eeba3a33cef8773a6ae552eda83
Sergey-2021 <tatarinov.serzhick@yandex.ru>
2021-11-15 10:55:47 +0300

    задача 29

---
+++ seminar 5 (24-36)/ex29/Program.cs
@@ -0,0 +1,16 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+
+System.Console.WriteLine();
+System.Console.WriteLine("Программа вычисления произведения чисел от 1 до N");
+System.Console.Write("Введите число N: ");
+int num = Convert.ToInt32(System.Console.ReadLine());
+int GetProd(int x)
+{
+    int s = 1;
+    for (int i = 1; i <= x; i++)
+    {
+        s *= i;
+    }
+    return s;
+}
+System.Console.WriteLine($"Произведение чисел от 1 до {num} = {GetProd(num)}");
