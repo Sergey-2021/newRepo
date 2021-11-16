4569cddd70ca0d12a7d83832c901428a2e7bbb0a
Sergey-2021 <tatarinov.serzhick@yandex.ru>
2021-11-16 07:49:20 +0300

    задача 37

---
+++ ex 37-47/ex 37/Program.cs
@@ -0,0 +1,35 @@
+﻿// В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99]
+
+int[] FillArray(int[] x)   // метод заполняет массив
+{
+    for (int i = 0; i < x.Length; i++)
+    {
+        x[i] = new Random().Next(10, 1000);
+    }
+    return x;
+}
+int GetCountElement(int[] x)  // метод находит количество элементов из отрезка [10, 99]
+{
+    int count = 0;
+    for (int i = 0; i < x.Length; i++)
+    {
+        if (x[i] >= 10 & x[i] <= 99)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+void PrintArrey(int[] x)
+{
+    for (int i = 0; i < x.Length; i++)
+    {
+        System.Console.Write(x[i] + " ");
+    }
+}
+
+int[] array = new int[123];
+array = FillArray(array);
+PrintArrey(array);
+System.Console.WriteLine();
+System.Console.WriteLine($"Количество элементов из отрезка в одномерном массиве из 123 чисел [10,99]: {GetCountElement(array)}");
