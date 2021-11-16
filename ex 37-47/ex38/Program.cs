5bb22d42bff999c8c4cd16abab62f53b8742ef8a
Sergey-2021 <tatarinov.serzhick@yandex.ru>
2021-11-16 08:03:26 +0300

    задача 38

---
+++ ex 37-47/ex38/Program.cs
@@ -0,0 +1,32 @@
+﻿// Найти сумму чисел одномерного массива стоящих на нечетной позиции
+int[] FillArray(int[] x, int a, int b)   // метод заполняет массив
+{
+    for (int i = 0; i < x.Length; i++)
+    {
+        x[i] = new Random().Next(a, b);
+    }
+    return x;
+}
+void PrintArrey(int[] x)
+{
+    for (int i = 0; i < x.Length; i++)
+    {
+        System.Console.Write(x[i] + " ");
+    }
+}
+int GetSumNegativElement(int[] x)
+{
+    int sum = 0;
+    for (int i = 1; i<x.Length; i+=2)
+    {
+        sum+=x[i];
+    }
+    return sum;
+}
+int[] array = new int[10];
+int from = -9;
+int before = 10;
+array = FillArray(array, from, before);
+PrintArrey(array);
+System.Console.WriteLine();
+System.Console.WriteLine($"Сумму чисел одномерного массива стоящих на нечетной позиции: {GetSumNegativElement(array)}");
