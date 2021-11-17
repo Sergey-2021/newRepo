73924a733895a1a28f24d42a98966879b68cd73b
Sergey-2021 <tatarinov.serzhick@yandex.ru>
2021-11-18 00:56:29 +0300

    задача 42

---
+++ ex 37-47/ex42/Program.cs
@@ -0,0 +1,22 @@
+﻿//Определить сколько чисел больше 0 введено с клавиатуры
+string number = Console.ReadLine();
+int GetCount(string x)
+{
+    int count = 0;
+    int a = 0;
+    for (int i = 0; i < x.Length; i++)
+    {
+        if (a == 0 & Char.IsDigit(x[i]))
+        {
+            a = 1;
+            count++;
+        }
+        else if (a >= 0 & !Char.IsDigit(x[i]))
+        {
+            a = 0;
+        }
+    }
+    return count;
+}
+
+System.Console.WriteLine($"Чисел больше 0 введено с клавиатуры: {GetCount(number)}");
