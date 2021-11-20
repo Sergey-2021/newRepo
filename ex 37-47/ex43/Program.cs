b2b91f6343192f262e297d70c8f5c48008b24383
Sergey-2021 <tatarinov.serzhick@yandex.ru>
2021-11-21 00:00:24 +0300

    задача 43

---
+++ ex 37-47/ex43/Program.cs
@@ -0,0 +1,29 @@
+﻿// Написать программу преобразования десятичного числа в двоичное
+System.Console.Write("Введите десятичное число: ");
+int num = int.Parse(Console.ReadLine());
+System.Console.Clear();
+string GetBinaryNumber(int x)
+{
+    string b = string.Empty;
+    string c = string.Empty;
+    for (int i = 0; x != 0; i++)
+    {
+        if (x == 1)
+        {
+            b += $"{1}";
+            x = 0;
+        }
+        else
+        {
+            b += $"{x % 2}";
+            x = x / 2;
+        }
+    }
+    for (int j = b.Length - 1; j >= 0; j--)
+    {
+        c += $"{b[j]}";
+    }
+    return c;
+}
+string binary = GetBinaryNumber(num);
+System.Console.WriteLine($"Двоичное представление числа {num} = {binary}");
