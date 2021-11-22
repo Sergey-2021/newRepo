cbb833fb451e0d16ea27ff8a3e65fda85ba15357
Sergey-2021 <tatarinov.serzhick@yandex.ru>
2021-11-22 07:18:38 +0300

    задача 45

---
+++ ex 37-47/ex45/Program.cs
@@ -0,0 +1,12 @@
+﻿// Показать числа Фибоначчи
+int a = 0;
+int b = 1;
+int n = 15;
+System.Console.Write(a + " ");
+System.Console.Write(b + " ");
+for (int i = 0; i < n; i++)
+{
+    System.Console.Write(a + b + " ");
+    b = a + b;
+    a = b - a;
+}
