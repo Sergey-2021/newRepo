9c7bbe92f72a5a474974d1e26abf8e8c1b8035d4
Sergey-2021 <tatarinov.serzhick@yandex.ru>
2021-12-10 01:41:50 +0300

    Добавил задачи 66 и 68

---
+++ Seminar/ex66-74/Program.cs
@@ -0,0 +1,46 @@
+﻿// 66. Показать натуральные числа от 1 до N, N задано
+void T(int x)
+{
+    if (x < 1) return;
+    T(x - 1);
+    System.Console.Write(x + " ");
+}
+
+
+void B(int x)
+{
+    if (x > 1)
+    {
+        T(x - 1);
+        System.Console.Write(x + " ");
+    }
+}
+System.Console.WriteLine();
+
+
+//68.Показать натуральные числа от M до N, N и M заданы
+
+void G(int m, int n)
+{
+    if (m < n)
+    {
+        if (n == m) System.Console.Write(n);
+
+        else
+        {
+            G(m, n - 1);
+            System.Console.Write(" " + n);
+        }
+    }
+    else if (m >= n)
+    {
+        if (m == n) System.Console.Write(m);
+
+        else
+        {
+            System.Console.Write(m + " ");
+            G(m - 1, n);
+        }
+    }
+}
+G(10, 5);
